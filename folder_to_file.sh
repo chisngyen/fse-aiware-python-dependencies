de62b95a32e073714344dd1d01fd364f91669e1a
Trần Chí Nguyên <tcnguyen2365@gmail.com>
2026-02-25 12:15:07 +0700

    chore: sync all files - remove old docs, add resolver output data
    - Remove obsolete docs: DAY_3_START.md, DEPENDENCY_RESOLUTION_TECHNIQUES.md,
      IMPLEMENTATION_CHECKLIST.md, PLLM_ANALYSIS.md, README_START_HERE.md,
      SESSION_SUMMARY.md, SYSTEM_DESIGN.md, old shell scripts, old docker logs
    - Add output_data yml files from resolver run (1501+ gists processed)
    - Update modified output_data yml files with new results

--- folder_to_file.sh
+++
@@ -1,8 +0,0 @@
-# Simple base script to create a list of folders into a file for running our gists
-#! /bin/bash
-
-folder_name=$1
-
-for item in "$folder_name"/*; do
-    echo $item >> my_gists.csv
-done
